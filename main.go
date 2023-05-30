b853d9ce6ab144ccb2b9b43d2ff5aa14dfd2b54e
mell <chanon4817@gmail.com>
2023-05-30 17:11:15 +0700

    feat: sending message

--- main.go
+++ main.go
@@ -7,11 +7,47 @@ import (
 )
 
 func main() {
+	fmt.Println("start")
 	connect, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
-		fmt.Println(err)
-		panic(1)
+		fmt.Println("failed init broker connection")
+		panic(err)
 	}
-	defer connect.Close()
+
+	ch, err := connect.Channel()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer ch.Close()
+
+	// dead letter queue - https://aws.amazon.com/what-is/dead-letter-queue/
+	q, err := ch.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	fmt.Println(q)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = ch.Publish(
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Hello World"),
+		},
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println("successfully connected to rabbitMQ")
+
 }
